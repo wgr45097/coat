1e09d53dda72258133d9002667d4b26371bd24a3
Guorui Wen <grwen@stu.pku.edu.cn>
2024-10-29 21:17:18 -0400

    use ccache for building dependencies

--- buildDependencies.sh
+++ buildDependencies.sh
@@ -7,21 +7,21 @@ git submodule update --init
 pushd external/asmjit
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER_LAUNCHER=ccache	-DCMAKE_CXX_COMPILER_LAUNCHER=ccache ..
 make $@
 popd
 
 pushd external/asmjit-utilities
 mkdir -p perf/build
 cd perf/build
-ASMJIT_ROOT=../../../external/asmjit cmake -DCMAKE_BUILD_TYPE=Release ..
+ASMJIT_ROOT=../../../external/asmjit cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache ..
 make $@
 popd
 
 pushd external/llvm/llvm
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_USE_PERF=ON ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_USE_PERF=ON -DCMAKE_C_COMPILER_LAUNCHER=ccache	-DCMAKE_CXX_COMPILER_LAUNCHER=ccache ..
 make $@
 make install
 popd
